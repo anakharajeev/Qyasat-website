420e307bc43bcbe83045f3f4a5c9cd7e1cc110e6
Annu <zmac@Zmacs-MacBook-Pro.local>
2024-04-24 22:37:30 +0530

    initial commit

---
+++ assets/js/script2.js
@@ -0,0 +1,19 @@
+
+	function handlePreloader() {
+		if($('.loader-wrap').length){
+			$('.loader-wrap').delay(1000).fadeOut(500);
+		}
+		TweenMax.to($(".loader-wrap .overlay"), 1.2, {
+            force3D: true,
+            left: "100%",
+            ease: Expo.easeInOut,
+        });
+	}
+
+
+	
+	$(window).on('load', function() {
+		handlePreloader();
+	
+	});	
+
